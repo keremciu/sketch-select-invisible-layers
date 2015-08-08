ec219fa74492d81a1610a818b5b222e15fe38c3a
Kerem Sevencan <kerem@ritmix.org>
2015-08-08 23:18:14 +0300

    helpers updated for annotations

--- helpers.js
+++ helpers.js
@@ -1,13 +1,16 @@
+// check the layer, it is a group or a layer
 function is_group(layer) {
   return [layer isMemberOfClass:[MSLayerGroup class]] || [layer isMemberOfClass:[MSArtboardGroup class]]
 }
 
+// if the layer is a invisible, select it
 function is_invisible_selectit(layer) {
 	if([layer isVisible] == false) {
 		[layer select:true byExpandingSelection:true]
 	}
 }
 
+// if the layer has a child, search its invisible children.
 function searchChilds(layers) {
 	for (var x=0; x < [layers count]; x++) {
 		var childLayer = layers.array()[x];
